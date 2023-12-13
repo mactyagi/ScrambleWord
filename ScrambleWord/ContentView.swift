4217855c44b5baab179e1097fe6fda970b64664e
manukant Tyagi <mactyagi@icloud.com>
2023-12-13 14:32:19 +0530

    Initial Commit

---
+++ ScrambleWord/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScrambleWord
+//
+//  Created by Manu on 13/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScrambleWord/ScrambleWordApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrambleWordApp.swift
+//  ScrambleWord
+//
+//  Created by Manu on 13/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrambleWordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
